b020f5a010990100eb9d4c0326984de9db15b2ed
Sonu Aryan <sonuaryan76448002@gmail.com>
2021-09-21 20:42:18 +0530

    way changed

--- include/stdint.h
+++ include/stdint.h
@@ -1,4 +1,15 @@
 #ifndef _Include_STD_INT_H
 #define _Include_STD_INT_H
-
+typedef unsigned int u32;
+typedef unsigned long u64;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long long int u128;
+typedef int i32;
+typedef long i64;
+typedef char i8;
+typedef short i16;
+typedef long long int i128;
+typedef float f32;
+typedef double f64;
 #endif
